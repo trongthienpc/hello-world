452c69e1539f83bc71fac4d39588e901f59204b0
ITPC <pcking60@gmail.com>
2023-06-29 16:34:38 +0700

    change-test-case

--- app.test.js
+++ app.test.js
@@ -5,6 +5,6 @@ describe("App", () => {
   it('should return "Welcome to Jenkins" when GET request is made to "/"', async () => {
     const response = await request(server).get("/");
     expect(response.status).toBe(200);
-    expect(response.text).toBe("Welcome to Jenkins");
+    expect(response.text).toBe("Welcome to Jenkins 1");
   });
 });
